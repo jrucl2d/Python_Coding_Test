260d0b3a9a4c2859ff42070deff78d0c2076c64d
jrucl2d <ysk789465@gmail.com>
2020-08-22 22:07:13 +0900

    P147 예제 5-9 BFS 예제 큐를 이용한 BFS 예제

--- 예제5-9.py
+++ 예제5-9.py
@@ -26,7 +26,7 @@ def BFS(index):
         for i in graph[index]:
             if not visited[i]:
                 q.append(i)
-                visited[i] = True
+                visited[i] = True  # 스택을 사용하는 DFS와는 다르게 여기서 방문처리 해줘야 함
 
 
 BFS(1)
