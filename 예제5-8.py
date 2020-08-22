f3c10af7b82b3de1ed49257155e6b193641717eb
jrucl2d <ysk789465@gmail.com>
2020-08-22 22:06:28 +0900

    P142 예제 5-8 DFS 예제 스택 대신 재귀를 이용한 DFS 풀이

--- 예제5-8.py
+++ 예제5-8.py
@@ -14,7 +14,7 @@ visited = [False] * 9
 
 
 def DFS(index):
-    visited[index] = True
+    visited[index] = True  # 원래 for문 안에서 방문 처리를 해야 하지만 바로 재귀로 오므로 여기서 해도 됨
     print(index, " 방문")
     for i in graph[index]:
         if not visited[i]:
