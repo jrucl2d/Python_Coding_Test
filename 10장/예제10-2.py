8130dc7cc60b243b0624d8d4afe7193a97285545
jrucl2d <ysk789465@gmail.com>
2020-09-08 16:01:04 +0900

    서로소 집합 알고리즘(경로 압축 적용)

---
+++ 10장/예제10-2.py
@@ -0,0 +1,38 @@
+# find 연산
+def find_parent(parent, x):
+    # 루트 노드가 아니면 부모를 갱신(경로 압축)
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+# union 연산
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:  # 더 큰 수가 작은 수를 가리키도록
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+v, e = map(int, input().split())
+parent = [0] * (v + 1)
+
+for i in range(1, v + 1):
+    parent[i] = i
+
+# union 연산을 수행
+for i in range(e):
+    a, b = map(int, input().split())
+    union_parent(parent, a, b)
+
+print('각 원소가 속한 집합: ', end=" ")
+for i in range(1, v + 1):
+    print(find_parent(parent, i), end=" ")
+
+print()
+
+print('부모 테이블 출력: ', end=" ")
+for i in range(1, v + 1):
+    print(parent[i], end=" ")
