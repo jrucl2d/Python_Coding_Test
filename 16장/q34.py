2024f59c311efbd021b7f067ed196115b6d08e2e
jrucl2d <ysk789465@gmail.com>
2020-12-27 12:29:38 +0900

    풀이 실패. LIS 문제인 것은 파악, 풀이 실패

---
+++ 16장/q34.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+dp = [1 for _ in range(n)]
+
+arr.reverse()
+
+for i in range(1, n):
+    for j in range(0, i):
+        if arr[i] > arr[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(n - max(dp))
